dadbf6c6d64872e84697d9d21da2bf07fda42f75
utsavmajhi <utsavmajhi@gmail.com>
2023-06-06 20:50:56 +0530

    Initial commit

---
+++ test/tele_health-be.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const TeleHealthBe = require('../lib/tele_health-be-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/tele_health-be-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new TeleHealthBe.TeleHealthBeStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
